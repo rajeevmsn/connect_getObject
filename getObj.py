56a2b321b53040aef1204ccfacc597549b53c6dd
Rajeev MYLAPALLI <rajeev.msn@gmail.com>
2024-07-23 13:48:05 +0200

    list of classes

--- getObj.py
+++ getObj.py
@@ -6,37 +6,36 @@ from dotenv import load_dotenv
 connectURL = 'https://connect-project.io'
 parseApplication = 'connect'
 
-# Load environment variables from .env file
-load_dotenv(".env")
+#folder name to save the data
+connectClasses = "connectClasses"
 
-# Get the sessionToken and Dataclass from environment variables
+load_dotenv(".env")
 sessionToken = os.getenv("sessionToken")
-dataClass = os.getenv("dataClass")
+dataClass = os.getenv("dataClass").split(",")
+format = os.getenv("format")
 
 print(f"sessionToken: {sessionToken}")
 print(f"dataClass: {dataClass}")
 
 def retrieveData(connectClass, connectToken, skip=0):
-    #Parameters for requests
+    """Retrieve data from the specified connect class."""
+    print(f"Retrieving {connectClass} data")
     url = f'{connectURL}/parse/classes/{connectClass}'
     headers = {
         'x-parse-application-id': parseApplication,
         'x-parse-session-token': connectToken,
     }
-
     params = {
         'limit': '100',
         'skip': str(skip),
     }
-
-    # Retrieve data
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()  # Raises an HTTPError for bad responses
     data = response.json()['results']
-
     return data
 
 def retrieveAllData(connectClass, connectToken):
+    """Retrieve all data from the specified connect class."""
     print(f"{'-'*10}Retrieving {connectClass} {'-'*10}")
     count=0
     allData = []
@@ -53,37 +52,44 @@ def retrieveAllData(connectClass, connectToken):
     
     return allData
 
-data = retrieveAllData(dataClass, sessionToken)
-print(len(data))
+def directoryCheck(directory):
+    """Check if the directory exists, if not create it."""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-df = pd.DataFrame(data)
-print(df.head(2))
-print(df.columns)
-
-print(f"Number of different userId: {len(df['userId'].unique())}")
-#print(df['userId'].tolist())
-userIdCount = df['userId'].value_counts()
-print(userIdCount)
-# Create the connectClasses folder
-connectClasses = "connectClasses"
-if not os.path.exists(connectClasses):
-    os.makedirs(connectClasses)
-
-# Create the dataClass folder inside connectClasses
-dataClassFolder = os.path.join(connectClasses, dataClass)
-if not os.path.exists(dataClassFolder):
-    os.makedirs(dataClassFolder)
-    # Split data based on applicationId and save in folder with applicationId name
-    df['applicationId'].fillna('Tests12345', inplace=True)
+def saveData(df, dataClassFolder):
+    print(f"Number of different userId: {len(df['userId'].unique())}")
+    userIdCount = df['userId'].value_counts()
+    print(userIdCount)
+    df['applicationId'] = df['applicationId'].fillna('Tests12345')
     for applicationId in df['applicationId'].unique():
         dfApp = df[df['applicationId'] == applicationId]
         print(f"Application ID: {applicationId}")
         folderPath = os.path.join(dataClassFolder, applicationId)
-        if not os.path.exists(folderPath):
-            os.makedirs(folderPath)
+        directoryCheck(folderPath)
         # Save the individual dataframes as CSV files in the folder
-        df['userId'].fillna('users12345', inplace=True)
+        df['userId'] = df['userId'].fillna('users12345')
         for userId in dfApp['userId'].unique():
             dfUser = dfApp[dfApp['userId'] == userId]
-            file_path = os.path.join(folderPath, f"{userId}.csv")
+            file_path = os.path.join(folderPath, f"{userId}.{format}")
             dfUser.to_csv(file_path, index=False)
+            print(f"File saved: {file_path}")
+
+
+def main():
+    directoryCheck(connectClasses)
+    for dataClassItem in dataClass:
+        dataClassFolder = os.path.join(connectClasses, dataClassItem)
+        directoryCheck(dataClassFolder)
+
+        data = retrieveAllData(dataClassItem, sessionToken)
+        print(len(data))
+
+        df = pd.DataFrame(data)
+        print(df.head(2))
+        print(df.columns)
+
+        saveData(df, dataClassFolder)
+
+if __name__ == '__main__':
+    main()
