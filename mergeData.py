81434118a8537210bf941feaf9108951210192c6
Rajeev MYLAPALLI <rajeev.msn@gmail.com>
2024-07-26 15:40:38 +0200

    splitData in mergeData.py and updated gitIgnore

--- mergeData.py
+++ mergeData.py
@@ -5,8 +5,10 @@ from getObj import directoryCheck
 bangleStreamPath = 'connectClasses/bangleStream28/eskQd0UPDC' #Path to the folder with bangle stream data
 annotationPath = 'connectClasses/userAnnotations/eskQd0UPDC' #Path to the folder with user annotations
 annotataionTime = 300000 #Time to mark the annotation
+calmData, stressedData = [], []
 
 directoryCheck('mergedData')
+directoryCheck('splitData')
 
 bangleCSVs = glob.glob(bangleStreamPath + '/*.csv')
 annotationCSVs = glob.glob(annotationPath + '/*.csv')
@@ -28,6 +30,60 @@ def associateBangleAnnotations(bangleData, annotationData, annotationTime):
 
     return mergedData
 
+
+def categorizeAnnotation(annotation):
+    if annotation == "calm":
+        return calmData
+    else: 
+        return stressedData
+
+def splitData(associated, name):
+    print("df shape: ", associated.shape)
+    name = name.split('.')[0]
+    nrows, _ = associated.shape
+    col = associated.columns
+
+    currentSeries = []
+    i, previousRow = 1, associated.iloc[0]
+    previousAnnotation = previousRow['annotations']
+    num = 1
+    while i < nrows:
+        row = associated.iloc[i]
+        annotation = row['annotations']
+
+        if annotation == previousAnnotation:
+            if annotation != None:
+                # currently recording an annotation
+                currentSeries.append(row)
+        elif annotation == None:
+            # stop an annotation record
+            df = pd.DataFrame(columns=col, data=currentSeries)
+            data = categorizeAnnotation(previousAnnotation)
+            data.append(df)
+            df.to_csv('splitData/' + name + str(num), index=False)
+            currentSeries = []
+            num += 1
+        elif previousAnnotation == None:
+            # start an annotation record
+            currentSeries.append(row)
+        else:
+            # stop and start an annotation record
+            df = pd.DataFrame(columns=col, data=currentSeries)
+            data = categorizeAnnotation(previousAnnotation)
+            data.append(df)
+            currentSeries = [row]
+
+        i += 1
+        previousRow = row
+        previousAnnotation = annotation
+
+    if annotation != None:
+        df = pd.DataFrame(columns=col, data=currentSeries)
+        data = categorizeAnnotation(annotation)
+        data.append(df)
+    
+    print(str(num) + " files created for " + name)
+
 for bangleCSV in bangleCSVs:
     csvName = bangleCSV.split('/')[-1]
     for annotationCSV in annotationCSVs:
@@ -39,6 +95,8 @@ for bangleCSV in bangleCSVs:
             mergedData = associateBangleAnnotations(bangleData, annotationsData, annotataionTime)
             print(mergedData['annotations'].value_counts())
             mergedData.to_csv('mergedData/' + csvName, index=False)
+            print("Splitting data for: ", csvName)
+            splitData(mergedData, csvName)
         else:
             continue
     
